2bc74af8d6c86bbaaa605ffcc7debb27f1146c4c
Parapangidis <98909010+Parapangidis@users.noreply.github.com>
2022-10-03 12:51:45 +0300

    Create a1f0.c

---
+++ Semester1/ProceduralProgramming/a1f0.c
@@ -0,0 +1,29 @@
+/*
+ * File name: a1f0.c
+ * ------------------
+ * Author: Christos Parapangidis, with direction from Alexandros Chatzigeorgiou
+ * Date: 3 Oct 2022
+ * This program adds two numbers
+ */
+#include <stdio.h>
+#include "simpio.h"
+#include "genlib.h"
+
+main () {
+    // declare variables
+    int n1, n2, total;
+
+    printf("This program adds two numbers\n");
+
+    printf("Please enter the first number: ");
+    n1 = GetInteger();
+
+    printf("Please enter the second number: ");
+    n2 = GetInteger();
+
+    total = n1 + n2;
+
+    printf("The sum is %d\n", total);
+  
+    return 0;
+}
