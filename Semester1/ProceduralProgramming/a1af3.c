50e5b1c06b5950047dbbdee2a7bc394d79872bf7
Parapangidis <98909010+Parapangidis@users.noreply.github.com>
2022-10-24 12:08:58 +0300

    Create a1af3.c

---
+++ Semester1/ProceduralProgramming/a1af3.c
@@ -0,0 +1,33 @@
+/*
+ * File name: a1af3.c
+ * ------------------
+ * Author: Christos Parapangidis
+ * Date: 24 Oct 2022
+ * This program calculates the years it will take for a specific amount of cars to surpass 160000
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+// Define constants per instructions
+#define INCREASING_RATE 0.07
+#define LIMIT 160000
+
+int main() {
+    // Declare variables
+    long yearsForCompletion, carsAmount;
+
+    // Initialize variables
+    yearsForCompletion = 0;
+    carsAmount = 80000;
+
+    while (carsAmount <= LIMIT) {
+        carsAmount = ceil(carsAmount * (1 + INCREASING_RATE)); //as per instructions
+        yearsForCompletion++;
+    }
+
+    printf("The years it took for the cars to surpass %d are: %ld\n", LIMIT, yearsForCompletion);
+    printf("The amount of cars in year %ld are: %ld\n", yearsForCompletion, carsAmount);
+
+    return 0;
+}
